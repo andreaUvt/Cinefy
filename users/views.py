13e83313bafeba51b3d0ed0f9012b7b3ccf2e06c
ChioibasDaniel <daniel.chioibas00@e-uvt.ro>
2024-05-09 14:03:04 +0300

    erori fix

--- users/views.py
+++ users/views.py
@@ -93,12 +93,13 @@ def registerUser(request):
             user.username = user.username.lower()
             user.is_active=False
             user.save()
-            errors=form.errors
+            
 
             activateEmail(request, user, form.cleaned_data.get('email'))
             return redirect('/')
         else:
-            for error in errors:
+            for error in form.errors:
                 messages.error(request, f'Error {error}')
+                
             
     return render(request,'users/login_register.html',context)
